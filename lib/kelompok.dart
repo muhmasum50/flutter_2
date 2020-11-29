1a04a62180dc20a0c927edeb5bb30b9eb670d1e4
akuPiXEL <70680916+pixel6661@users.noreply.github.com>
2020-11-29 20:40:02 +0700

    gabung page 1, page 2 dan page3

--- lib/kelompok.dart
+++ lib/kelompok.dart
@@ -1,71 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:math';
 
-void main() {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: new Home(),
-    title: 'UTS',
-    routes: <String, WidgetBuilder>{
-      '/home': (BuildContext context) => new Home(),
-      '/kelompok': (BuildContext context) => new Kelompok(),
-      '/luas': (BuildContext context) => new Luas(),
-      
-    },
-  ));
+// void main() => runApp(Kelompok());
+
+class Kelompok extends StatefulWidget {
+  @override
+  _KelompokState createState() => _KelompokState();
 }
 
-class Home extends StatelessWidget {
+class _KelompokState extends State<Kelompok> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hitung Luas Lingkaran"),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Container(
-          padding: new EdgeInsets.all(22.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(width: 10),
-              Image.asset('assets/lingkaran.jpg'),
-              Text(
-                "Tentang Kami",
-                style: TextStyle(fontSize: 20.0),
-              ),
-              Text(
-                  "ini adalah aplikasi menghitung luas lingkaran yang bertujuan untuk memudahkan menghitung luas lingkuran. dan aplikasi ini dibuat untuk projek uts"),
-              RaisedButton(
-                  child: Text(
-                    "Halaman Selanjutnya",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  color: Colors.blue,
-                  onPressed: () =>
-                      Navigator.of(context).pushNamed('/kelompok')),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Kelompok extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
         title: 'Profil Kelompok 4',
         home: Scaffold(
           appBar: AppBar(
             title: Text('Profil Kelompok 4'),
-            backgroundColor: Colors.blueAccent,
+            backgroundColor: Colors.amber,
           ),
           body: Column(
+//mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(padding: EdgeInsets.only(top: 20.0)),
               Text(
@@ -113,7 +67,7 @@ class Kelompok extends StatelessWidget {
               Container(
                 padding: EdgeInsets.only(left: 32, right: 32),
                 child: Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     SizedBox(
                         //CIRCLE FOTO
@@ -150,6 +104,7 @@ class Kelompok extends StatelessWidget {
               Container(
                 padding: EdgeInsets.only(left: 32, right: 32),
                 child: Row(
+//mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     SizedBox(
                         //CIRCLE FOTO
@@ -221,128 +176,12 @@ class Kelompok extends StatelessWidget {
                 ),
               ),
               RaisedButton(
-                child: Text(
-                  "Halaman Selanjutnya",
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Colors.blue,
-                onPressed: () =>
-                    Navigator.of(context).pushNamed('/luas')
-              ),
-              RaisedButton(
-                child: Text(
-                  "Halaman Sebelumnya",
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Colors.blue,
-                onPressed: () =>
-                    Navigator.of(context).pushNamed('/home')
-              ),
+                  child: Text('Kembali'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
             ],
           ),
         ));
-}
-}
-
-class Luas extends StatefulWidget {
-  @override
-  _LuasState createState() => _LuasState();
-}
-class _LuasState extends State<Luas> {
-
-  double hasil = 0;
-
-  TextEditingController phi = new TextEditingController();
-  TextEditingController jariJari = new TextEditingController();
-
-  void _calculate() {
-    setState(() {
-      double valJari = double.parse(jariJari.text);
-      double valPhi = double.parse(phi.text);
-
-      hasil = (pow(valJari, 2) * valPhi);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hitung Luas Lingkaran"),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Form(
-              child: formElement(),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget formElement() {
-    return Column(
-      children: <Widget>[
-        SizedBox(height: 20,),
-        TextFormField(
-          keyboardType: TextInputType.number,
-          controller: phi,
-          decoration: InputDecoration(
-            labelText: "Input phi",
-            hintText: "Masukan value phi",
-            border: OutlineInputBorder()
-          ),
-        ),
-
-        SizedBox(height: 20,),
-
-        TextFormField(
-          keyboardType: TextInputType.number,
-          controller: jariJari,
-          decoration: InputDecoration(
-              labelText: "Input jari jari",
-              hintText: "Masukan value jari jari",
-              border: OutlineInputBorder()
-          ),
-        ),
-        new Divider(),
-
-        Card(
-          color: Colors.lightBlueAccent,
-          elevation: 0,
-          child: Container(
-            // width: MediaQuery.of(context).size.width,
-            height: 45,
-            child: InkWell(
-              splashColor: Colors.blueGrey,
-              child: Center(
-                child: Text("Hitung", style: TextStyle(color: Colors.white),),
-              ),
-              onTap: _calculate
-            ),
-          ),
-        ),
-
-        Text(
-          '$hasil',
-          // 'text',
-          style: TextStyle(fontSize: 45),
-        ),
-        RaisedButton(
-          child: Text(
-            "Halaman Sebelumnya",
-            style: TextStyle(color: Colors.white),
-          ),
-          color: Colors.blue,
-          onPressed: () =>
-              Navigator.of(context).pushNamed('/kelompok')),
-      ],
-    );
   }
 }
---
+++ lib/luas.dart
@@ -0,0 +1,111 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// void main() => runApp(Luas());
+
+class Luas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Hitung Luas Lingkaran",
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent
+      ),
+      home: HomeHitungLuasLingkaran(),
+    );
+  }
+}
+
+class HomeHitungLuasLingkaran extends StatefulWidget {
+  @override
+  _HomeHitungLuasLingkaranState createState() => _HomeHitungLuasLingkaranState();
+}
+
+class _HomeHitungLuasLingkaranState extends State<HomeHitungLuasLingkaran> {
+  double hasil = 0;
+
+  TextEditingController phi = new TextEditingController();
+  TextEditingController jariJari = new TextEditingController();
+
+  void _calculate() {
+    setState(() {
+      double valJari = double.parse(jariJari.text);
+      double valPhi = double.parse(phi.text);
+
+      hasil = (pow(valJari, 2) * valPhi);
+    });
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hitung Luas Lingkaran", style: TextStyle(fontWeight: FontWeight.w600),),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Form(
+            child: formElement(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget formElement() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20,),
+        TextFormField(
+          keyboardType: TextInputType.number,
+          controller: phi,
+          decoration: InputDecoration(
+            labelText: "Input phi",
+            hintText: "Masukan value phi",
+            border: OutlineInputBorder()
+          ),
+        ),
+
+        SizedBox(height: 20,),
+
+        TextFormField(
+          keyboardType: TextInputType.number,
+          controller: jariJari,
+          decoration: InputDecoration(
+              labelText: "Input jari jari",
+              hintText: "Masukan value jari jari",
+              border: OutlineInputBorder()
+          ),
+        ),
+        new Divider(),
+
+        Card(
+          color: Colors.lightBlueAccent,
+          elevation: 0,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 45,
+            child: InkWell(
+              splashColor: Colors.blueGrey,
+              child: Center(
+                child: Text("Hitung", style: TextStyle(color: Colors.white),),
+              ),
+              onTap: _calculate
+            ),
+          ),
+        ),
+
+        Text(
+          '$hasil',
+          style: TextStyle(fontSize: 45),
+        )
+      ],
+    );
+  }
+}
+
